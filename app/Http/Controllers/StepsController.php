c4ed04954268358cccbcf0c235f6e7eb80effa1a
Matzerath <matzerath@outlook.com>
2017-10-29 22:28:26 -0600

    steps

--- app/Http/Controllers/StepsController.php
+++ app/Http/Controllers/StepsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 
 class StepsController extends Controller
 {
